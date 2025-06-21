bcbfedf83d7135e2ee1d0d2602daa38e568cc298
Joshua Hirakawa <joshuahirakawa@gmail.com>
2025-06-21 10:53:02 -0700

    base filestructure for frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='p-4'>
+      <h1>Sidebar BABBYYYYY</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets.tsx
@@ -0,0 +1,1 @@
+// maybe for generated svgs
